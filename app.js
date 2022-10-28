0750cf122b2b8e35941b47a770e292297abada65
Karimi <himatanuj.karimi.ext@nokia.com>
2022-10-28 11:14:53 +0530

    FeedBack Form

---
+++ app.js
@@ -0,0 +1,18 @@
+let empty = []
+
+function submit(){
+
+    let temprev = document.getElementById('text').value;
+    let temprating = document.getElementById('rate').value;
+
+
+ let people = {
+
+    Review: temprev,
+    Rating: temprating,
+ }
+  empty.push(people);
+ 
+}
+
+
